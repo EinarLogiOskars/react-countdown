2368fcec2fb60f721cd4a24366d7150297b917ae
Einar Logi Óskarsson <einarlogi1@gmail.com>
2023-10-20 21:14:01 +0000

    Added functionality to chose date to countdown to

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import CountdownTimer from './CountdownTimer';
+import { useState } from 'react';
+import DateTimePicker from 'react-datetime-picker';
 
 import './App.css';
 
@@ -8,11 +10,32 @@ export default function App() {
   const NOW_IN_MS = new Date().getTime();
 
   const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+  const [targetDate, setTargetDate] = useState(new Date(dateTimeAfterThreeDays));
+
+  const handleChange = (event) => {
+    
+    event.preventDefault();
+    if (event.target.value) {
+      setTargetDate(new Date(event.target.value));
+    } else {
+      setTargetDate(new Date(dateTimeAfterThreeDays));
+    }
+    
+  };
 
   return (
     <div>
       <h1>Countdown Timer</h1>
-      <CountdownTimer targetDate={dateTimeAfterThreeDays} />
+      <form>
+        <label htmlFor='countdown-date-time'>Select a Date and Time: </label>
+        <input
+          id="countdown-date-time"
+          name="countdown-date-time"
+          type="datetime-local"
+          onChange={handleChange}
+          />
+      </form>
+      <CountdownTimer targetDate={targetDate} />
     </div>
   );
 }
--- src/CountdownTimer.js
+++ src/CountdownTimer.js
@@ -14,10 +14,7 @@ const ExpiredNotice = () => {
 const ShowCounter = ({ days, hours, minutes, seconds }) => {
   return (
     <div className="show-counter">
-      <a
-        href="https://tapasadhikary.com"
-        target="_blank"
-        rel="noopener noreferrer"
+      <div
         className="countdown-link"
       >
         <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
@@ -27,7 +24,7 @@ const ShowCounter = ({ days, hours, minutes, seconds }) => {
         <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
         <p>:</p>
         <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
-      </a>
+      </div>
     </div>
   );
 };
